dc5ffc55917f95692281029e8a99b6fab9840b15
Alifoxy <nightfoxy666@gmail.com>
2023-03-08 14:06:36 +0300

    first

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -4,5 +4,5 @@ config();
 
 export const configs = {
     PORT: process.env.PORT || 5001,
-    DB_URL: process.env.DB_URL || "jfsdlkfjdsaklfdas",
+    DB_URL: process.env.DB_URL || `mongodb+srv://Alifoxy:<nf380676045540>@cluster0.isch7bg.mongodb.net/?retryWrites=true&w=majority`,
 };
